b5a8bdfb7ef9bffe86e4da770739856579c93fa5
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2024-09-23 15:55:29 +0100

    Lesson02: JavaScript Introduction

---
+++ lesson02/HelloWorld.kt
@@ -0,0 +1,13 @@
+class A(var x: Int) {}
+
+
+fun main() {
+    println("Olá Glorioso mundo onde o Maior clube do mundo existe e tem mais títulos que todos os outros")
+    var z: int = 10
+
+    z = "SLB" 
+
+}
+
+
+
---
+++ lesson02/hello-world.js
@@ -0,0 +1,23 @@
+console.log("SLB")
+
+// class A {
+//     constructor(x) {
+//         this.x = x
+//     }
+// }
+
+// const a = new A(5)
+
+// a.X = 10
+
+// console.log(a.x)
+// console.log(a)
+
+// //console.log = "SLB"
+
+// console.log(a)
+
+// const b = 123
+
+
+
